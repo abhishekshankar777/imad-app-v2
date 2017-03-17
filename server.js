4d215f2f97883fc346edc36cb1bfae7bcdca3a2e
abhishekshankar777 <abhishekshankar7@gmail.com>
2017-03-17 21:19:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg'),Pool;
 
+var config = {
+    user: 'abhishekshankar777',
+    database: 'abhishekshankar777',
+    host:'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 var articles= {
@@ -82,6 +90,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   });
   
+  var pool = new Pool(config);
+  app.get('/test-db',function (req, res) {
+      pool.query('SELECT * FROM TEST',function(err,result){
+          if(err) {
+              res.status(500).send(err.toString());
+              
+          } else {
+              res.send(JSON,stringify(result));
+              
+          }
+      });
+      
+      
+  });
   
   
   
