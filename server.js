fbdb70fc631c289b7624e02f46bf6cda639f892d
abhishekshankar777 <abhishekshankar7@gmail.com>
2017-03-22 00:33:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ app.get('/counter', function(req, res) {
 });
 
 
-app.get('/articles/article-one',function(req,res)  {
+app.get('/articles/:article-one',function(req,res)  {
     pool.query("SELECT * FROM 'article' WHERE title= '", + req.params.articleName+ "'", function(err,result){
         if (err) {
             res.status(500).send(err.toString());
