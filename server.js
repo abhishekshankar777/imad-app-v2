0576d91cf2a81899825f2368626897e207746073
abhishekshankar777 <abhishekshankar7@gmail.com>
2017-03-21 21:54:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,13 +118,13 @@ app.get('/articles/article-one',function(req,res)  {
         if (err) {
             res.status(500).send(err.toString());
         } else {
-            if(result.rows.length===0) {
+            if(result.rows.length === 0) {
                 res.status(404).send('article not found');
             } else {
                 var articleData = result.rows[0];
                 res.send(createTemplate(articleData));
             }
-            }
+        }
     
     });
     });
