1be272fe51dd63babca5c3f3c4780dfaf6c688c8
abhishekshankar777 <abhishekshankar7@gmail.com>
2017-03-22 17:52:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool = require('pg').Pool;
-var Pool = require('pg').Pool;
+
 var crypto = require('crypto');
 var config = {
     user: 'abhishekshankar777',
@@ -95,7 +95,7 @@ app.get('/', function (req, res) {
   
   function hash(input,salt) {
     
-    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    var hashed = crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return hashed.toString('hex');
       
   }
@@ -103,7 +103,7 @@ app.get('/', function (req, res) {
   
   
   app.get('/hash/:input', function (req,res){
-      var hashedString =hash(req.params.input,'this-is-some-random-string');
+      var hashedString = hash(req.params.input,'this-is-some-random-string');
       res.send(hashedString);
     
   });
