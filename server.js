f5af23732eedc56fd5f455c4cb8e390d7977b499
abhishekshankar777 <abhishekshankar7@gmail.com>
2017-03-22 19:35:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,6 +128,48 @@ app.get('/', function (req, res) {
         });
     });
   
+  app.post('/login',function(req,res) {
+    var username = req.body.username;
+      var password = req.body.password;
+     
+      
+      pool.query('SELECT * from "user" username= $1',[username],function (err,result){
+          if(err) {
+              res.status(500).send(err.toString());
+              
+          } else {
+             
+           if(result.rows.length === 0) {
+                res.send(403).send('username/password is invalid'); 
+           }else {
+               var  dbString = result.rows[0].password;
+               var salt = dbString.split('$')[2];
+               var hashedpassword = hash(password, salt);
+               if (hashedpassword === dbString) {
+                   res.send('credentials correct!');
+               } else {
+                  res.send(403).send('username/password is invalid'); 
+               }
+           }
+          }
+      });
+      });
+         
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+  
    var pool = new Pool(config);
   app.get('/test-db',function (req, res) {
       pool.query('SELECT * FROM TEST',function(err,result){
