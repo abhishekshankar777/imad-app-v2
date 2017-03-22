e9891e4d2ca3e773882e74e2d07b2ba76c595b1d
abhishekshankar777 <abhishekshankar7@gmail.com>
2017-03-22 20:08:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,35 @@
-var button=document.getElementById('counter');
-var counter=0;
-
-
-button.onclick = function () {
+var submit = document.getElementById('submit_btn');
+submit.onclick =function () {
+    
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200) {
+                alert('Logged in successfully');
+            } else if (request.status === 403) {
+                alert('username/password is invalid');
+            }else if (request.status === 500) {
+                alert('something went wrong on the server');
+            }
+        }
+    };
     
     
     
     
     
-    counter= counter + 1;
-    var span= document.getElementById('count');
-    span.innerHTML = counter.toString();
+    
+    
+    
+    
+     var username =document.getElementById('username').value;
+      var password =document.getElementById('password').value;
+      console.log(username);
+      console.log(password);
+      request.open('POST','http://abhishekshankar777.imad.hasura-app.io/login',true);
+      request.setRequestHeader('Content-Type','application/json');
+      request.send(JSON.stringify({username: username, password: password}));
+      
 };
-    
     
