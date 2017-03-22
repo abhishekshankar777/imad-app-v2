5247e6ea3a397d5f18cc1decdd525768ab518b17
abhishekshankar777 <abhishekshankar7@gmail.com>
2017-03-22 19:10:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.get('/', function (req, res) {
   
   app.post('/create-user',function(req,res){
       var username = req.body.username;
-      var pasword = req.body.password;
+      var password = req.body.password;
      
       var salt = crypto.randomBytes(128).toString('hex');
       var dbString =hash(password,salt);
