ad628c14ae12b139228eebba8e775d142403d26e
abhishekshankar777 <abhishekshankar7@gmail.com>
2017-03-24 19:42:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config = {
     database: 'abhishekshankar777',
     host:'db.imad.hasura-app.io',
     port: '5432',
-    password: process.env.DB_PASSWORD
+    password: 'db-abhishekshankar777-84933'
 };
 var app = express();
 app.use(morgan('combined'));
